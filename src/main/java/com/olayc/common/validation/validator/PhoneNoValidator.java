8c2e608efb8ddea2bd9e130cf714d1b3d86c053e
LiJianBiao <lijianbiao@olasharing.com>
2019-07-06 11:45:19 +0800

    [dev] fixbug phone check reg

--- src/main/java/com/olayc/common/validation/validator/PhoneNoValidator.java
+++ src/main/java/com/olayc/common/validation/validator/PhoneNoValidator.java
@@ -8,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PhoneNoValidator implements ConstraintValidator<PhoneNo, String> {
 
-	private final String phoneNoRegex = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\\\d{8}$";
+	private final String phoneNoRegex = "^1([1-9]\\d|5[0-35-9]|7[3678])\\d{8}$";
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
